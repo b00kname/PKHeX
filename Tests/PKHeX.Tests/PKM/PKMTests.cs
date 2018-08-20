7849e17376a26913b554315d38ea20bb4c67877f
kkloh98 <40446720+kkloh98@users.noreply.github.com>
2018-08-20 22:37:01 +0800

    minor changes

--- Tests/PKHeX.Tests/PKM/PKMTests.cs
+++ Tests/PKHeX.Tests/PKM/PKMTests.cs
@@ -153,9 +153,9 @@ namespace PKHeX.Tests
             // -- Act
             pk.EggMetDate = null;
             // -- Assert
-            Assert.AreEqual(0, pk.Egg_Day, "Egg_Day was not zeroed when EggMetDate is set to null");
-            Assert.AreEqual(0, pk.Egg_Month, "Egg_Month was not zeroed when EggMetDate is set to null");
-            Assert.AreEqual(0, pk.Egg_Year, "Egg_Year was not zeroed when EggMetDate is set to null");
+            Assert.AreEqual(0, pk.Egg_Day, "Egg_Day was not zero-ed when EggMetDate is set to null");
+            Assert.AreEqual(0, pk.Egg_Month, "Egg_Month was not zero-ed when EggMetDate is set to null");
+            Assert.AreEqual(0, pk.Egg_Year, "Egg_Year was not zero-ed when EggMetDate is set to null");
 
             // Ensure setting to a date sets the components
             var now = DateTime.UtcNow;
