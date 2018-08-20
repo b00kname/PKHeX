10c04bd99fc124c1905b8ac2b90cccea9b98c6e1
kkloh98 <40446720+kkloh98@users.noreply.github.com>
2018-08-20 22:36:07 +0800

    grammar enhancing
    added "-" between zero and ed

--- Tests/PKHeX.Tests/PKM/PKMTests.cs
+++ Tests/PKHeX.Tests/PKM/PKMTests.cs
@@ -96,9 +96,9 @@ namespace PKHeX.Tests
             // -- Act
             pk.MetDate = null;
             // -- Assert
-            Assert.AreEqual(0, pk.Met_Day, "Met_Day was not zeroed when MetDate is set to null");
-            Assert.AreEqual(0, pk.Met_Month, "Met_Month was not zeroed when MetDate is set to null");
-            Assert.AreEqual(0, pk.Met_Year, "Met_Year was not zeroed when MetDate is set to null");
+            Assert.AreEqual(0, pk.Met_Day, "Met_Day was not zero-ed when MetDate is set to null");
+            Assert.AreEqual(0, pk.Met_Month, "Met_Month was not zero-ed when MetDate is set to null");
+            Assert.AreEqual(0, pk.Met_Year, "Met_Year was not zero-ed when MetDate is set to null");
 
             // Ensure setting to a date sets the components
             var now = DateTime.UtcNow;
